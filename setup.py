8689f84416a7f16845e0b3081ed2ef9a5bf984af
manash <manashn381@gmail.com>
2025-01-21 14:52:57 +0530

    logging and exception

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ def get_requirements(file_path:str)->List[str]: # this functions will return a l
     This function will return the list of requirements
     '''
     HYPEN_E_DOT="-e ."
-    requiremetns = []
+    requirements = []
     with open(file_path) as file_obj:
-        requiremetns=file_obj.readlines()
-        requiremetns=[req.replace("\n", "") for req in requiremetns]
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n", "") for req in requirements]
 
-        if HYPEN_E_DOT in requiremetns:
-            requiremetns.remove(HYPEN_E_DOT)
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
 
-    return requiremetns
+    return requirements
 setup(
     name='MLproject',
     version='0.0.1',
